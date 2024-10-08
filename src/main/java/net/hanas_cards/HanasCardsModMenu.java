cafee38ee327933485ae6c6c5c11c77fcd943be0
ThievishJoke <hanayokoikimo@gmail.com>
2024-10-08 08:36:48 -0400

    added comments in anticipation to add a config

---
+++ src/main/java/net/hanas_cards/HanasCardsModMenu.java
@@ -0,0 +1,11 @@
+package net.hanas_cards;
+
+//@Environment(EnvType.CLIENT)
+//public class HanasCardsModMenu implements ModMenuApi {
+//
+//    @Override
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return parent -> AutoConfig.getConfigScreen(HanasCardsConfig.class, parent).get();
+//    }
+//
+//}
